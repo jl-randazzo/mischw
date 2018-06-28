59eba843d019f5e43444732dae0bc4f484ca3f93
Luke <jl_randazzo@hotmail.com>
2018-06-28 16:50:56 -0600

    started building out the scheduler. Create logic for starting new processes

--- OSHW3/CPU.cc
+++ OSHW3/CPU.cc
@@ -21,44 +21,44 @@ If compiled with -DEBUG, when run it should produce the following
 output (approximately):
 
 $ ./a.out
-state:        3
-name:         IDLE
-pid:          21145
-ppid:         21143
+state:		3
+name:		 IDLE
+pid:		  21145
+ppid:		 21143
 interrupts:   0
-switches:     0
-started:      0
+switches:	 0
+started:	  0
 in CPU.cc at 240 at beginning of send_signals getpid() = 21144
 in CPU.cc at 245 sending signal = 14
 in CPU.cc at 246 to pid = 21143
-In ISR stopped:     21145
+In ISR stopped:	 21145
 ---- entering scheduler
-continuing    21145
+continuing	21145
 ---- leaving scheduler
 in CPU.cc at 245 sending signal = 14
 in CPU.cc at 246 to pid = 21143
-In ISR stopped:     21145
+In ISR stopped:	 21145
 ---- entering scheduler
-continuing    21145
+continuing	21145
 ---- leaving scheduler
 in CPU.cc at 245 sending signal = 14
 in CPU.cc at 246 to pid = 21143
-In ISR stopped:     21145
+In ISR stopped:	 21145
 ---- entering scheduler
-continuing    21145
+continuing	21145
 ---- leaving scheduler
 in CPU.cc at 245 sending signal = 14
 in CPU.cc at 246 to pid = 21143
-In ISR stopped:     21145
+In ISR stopped:	 21145
 ---- entering scheduler
-continuing    21145
+continuing	21145
 ---- leaving scheduler
 in CPU.cc at 245 sending signal = 14
 in CPU.cc at 246 to pid = 21143
 in CPU.cc at 250 at end of send_signals
-In ISR stopped:     21145
+In ISR stopped:	 21145
 ---- entering scheduler
-continuing    21145
+continuing	21145
 Terminated: 15
 ---------------------------------------------------------------------------
 Add the following functionality.
@@ -71,21 +71,21 @@ Add the following functionality.
 3) When a SIGALRM arrives, scheduler() will be called; it currently simply
    restarts the idle process. Instead, do the following.
    a) Update the PCB for the process that was interrupted including the
-      number of context switches and interrupts it had and changing its
-      state from RUNNING to READY.
+	  number of context switches and interrupts it had and changing its
+	  state from RUNNING to READY.
    b) If there are any NEW processes on processes list, change its state to
-      RUNNING, and fork() and execl() it.
+	  RUNNING, and fork() and execl() it.
    c) If there are no NEW processes, round robin the processes in the
-      processes queue that are READY. If no process is READY in the
-      list, execute the idle process.
+	  processes queue that are READY. If no process is READY in the
+	  list, execute the idle process.
 
 4) When a SIGCHLD arrives notifying that a child has exited, process_done() is
    called. process_done() currently only prints out the PID and the status.
    a) Add the printing of the information in the PCB including the number
-      of times it was interrupted, the number of times it was context
-      switched (this may be fewer than the interrupts if a process
-      becomes the only non-idle process in the ready queue), and the total
-      system time the process took.
+	  of times it was interrupted, the number of times it was context
+	  switched (this may be fewer than the interrupts if a process
+	  becomes the only non-idle process in the ready queue), and the total
+	  system time the process took.
    b) Change the state to TERMINATED.
    c) Restart the idle process to use the rest of the time slice.
 */
@@ -94,19 +94,19 @@ Add the following functionality.
 #define EVER ;;
 
 #define assertsyscall(x, y) if(!((x) y)){int err = errno; \
-    fprintf(stderr, "In file %s at line %d: ", __FILE__, __LINE__); \
-        perror(#x); exit(err);}
+	fprintf(stderr, "In file %s at line %d: ", __FILE__, __LINE__); \
+		perror(#x); exit(err);}
 
 #ifdef EBUG
 #   define dmess(a) cout << "in " << __FILE__ << \
-    " at " << __LINE__ << " " << a << endl;
+	" at " << __LINE__ << " " << a << endl;
 
 #   define dprint(a) cout << "in " << __FILE__ << \
-    " at " << __LINE__ << " " << (#a) << " = " << a << endl;
+	" at " << __LINE__ << " " << (#a) << " = " << a << endl;
 
 #   define dprintt(a,b) cout << "in " << __FILE__ << \
-    " at " << __LINE__ << " " << a << " " << (#b) << " = " \
-    << b << endl
+	" at " << __LINE__ << " " << a << " " << (#b) << " = " \
+	<< b << endl
 #else
 #   define dmess(a)
 #   define dprint(a)
@@ -124,13 +124,13 @@ enum STATE { NEW, RUNNING, WAITING, READY, TERMINATED };
 
 struct PCB
 {
-    STATE state;
-    const char *name;   // name of the executable
-    int pid;            // process id from fork();
-    int ppid;           // parent process id
-    int interrupts;     // number of times interrupted
-    int switches;       // may be < interrupts
-    int started;        // the time this process started
+	STATE state;
+	const char *name;   // name of the executable
+	int pid;			// process id from fork();
+	int ppid;		   // parent process id
+	int interrupts;	 // number of times interrupted
+	int switches;	   // may be < interrupts
+	int started;		// the time this process started
 };
 
 PCB *running;
@@ -151,35 +151,35 @@ int sys_time;
 */
 int eye2eh(int i, char *buf, int bufsize, int base)
 {
-    if(bufsize < 1) return(-1);
-    buf[bufsize-1] = '\0';
-    if(bufsize == 1) return(0);
-    if(base < 2 || base > 16)
-    {
-        for(int j = bufsize-2; j >= 0; j--)
-        {
-            buf[j] = ' ';
-        }
-        return(-1);
-    }
+	if(bufsize < 1) return(-1);
+	buf[bufsize-1] = '\0';
+	if(bufsize == 1) return(0);
+	if(base < 2 || base > 16)
+	{
+		for(int j = bufsize-2; j >= 0; j--)
+		{
+			buf[j] = ' ';
+		}
+		return(-1);
+	}
 
-    int count = 0;
-    const char *digits = "0123456789ABCDEF";
-    for(int j = bufsize-2; j >= 0; j--)
-    {
-        if(i == 0)
-        {
-            buf[j] = ' ';
-        }
-        else
-        {
-            buf[j] = digits[i%base];
-            i = i/base;
-            count++;
-        }
-    }
-    if(i != 0) return(-1);
-    return(count);
+	int count = 0;
+	const char *digits = "0123456789ABCDEF";
+	for(int j = bufsize-2; j >= 0; j--)
+	{
+		if(i == 0)
+		{
+			buf[j] = ' ';
+		}
+		else
+		{
+			buf[j] = digits[i%base];
+			i = i/base;
+			count++;
+		}
+	}
+	if(i != 0) return(-1);
+	return(count);
 }
 
 /*
@@ -190,7 +190,7 @@ void grab(int signum) { WRITEI(signum); WRITES("\n"); }
 
 // c++decl> declare ISV as array 32 of pointer to function(int) returning void
 void(*ISV[32])(int) = {
-/*        00    01    02    03    04    05    06    07    08    09 */
+/*		00	01	02	03	04	05	06	07	08	09 */
 /*  0 */ grab, grab, grab, grab, grab, grab, grab, grab, grab, grab,
 /* 10 */ grab, grab, grab, grab, grab, grab, grab, grab, grab, grab,
 /* 20 */ grab, grab, grab, grab, grab, grab, grab, grab, grab, grab,
@@ -202,23 +202,23 @@ void(*ISV[32])(int) = {
 */
 void ISR(int signum)
 {
-    if(signum != SIGCHLD)
-    {
-        if(kill(running->pid, SIGSTOP) == -1)
-        {
-            WRITES("In ISR kill returned: ");
-            WRITEI(errno);
-            WRITES("\n");
-            return;
-        }
+	if(signum != SIGCHLD)
+	{
+		if(kill(running->pid, SIGSTOP) == -1)
+		{
+			WRITES("In ISR kill returned: ");
+			WRITEI(errno);
+			WRITES("\n");
+			return;
+		}
 
-        WRITES("In ISR stopped: ");
-        WRITEI(running->pid);
-        WRITES("\n");
-        running->state = READY;
-    }
+		WRITES("In ISR stopped: ");
+		WRITEI(running->pid);
+		WRITES("\n");
+		running->state = READY;
+	}
 
-    ISV[signum](signum);
+	ISV[signum](signum);
 }
 
 /*
@@ -226,14 +226,14 @@ void ISR(int signum)
 */
 ostream& operator <<(ostream &os, struct PCB *pcb)
 {
-    os << "state:        " << pcb->state << endl;
-    os << "name:         " << pcb->name << endl;
-    os << "pid:          " << pcb->pid << endl;
-    os << "ppid:         " << pcb->ppid << endl;
-    os << "interrupts:   " << pcb->interrupts << endl;
-    os << "switches:     " << pcb->switches << endl;
-    os << "started:      " << pcb->started << endl;
-    return(os);
+	os << "state:		" << pcb->state << endl;
+	os << "name:		 " << pcb->name << endl;
+	os << "pid:		  " << pcb->pid << endl;
+	os << "ppid:		 " << pcb->ppid << endl;
+	os << "interrupts:   " << pcb->interrupts << endl;
+	os << "switches:	 " << pcb->switches << endl;
+	os << "started:	  " << pcb->started << endl;
+	return(os);
 }
 
 /*
@@ -241,12 +241,12 @@ ostream& operator <<(ostream &os, struct PCB *pcb)
 */
 ostream& operator <<(ostream &os, list<PCB *> which)
 {
-    list<PCB *>::iterator PCB_iter;
-    for(PCB_iter = which.begin(); PCB_iter != which.end(); PCB_iter++)
-    {
-        os <<(*PCB_iter);
-    }
-    return(os);
+	list<PCB *>::iterator PCB_iter;
+	for(PCB_iter = which.begin(); PCB_iter != which.end(); PCB_iter++)
+	{
+		os <<(*PCB_iter);
+	}
+	return(os);
 }
 
 /*
@@ -254,24 +254,24 @@ ostream& operator <<(ostream &os, list<PCB *> which)
 */
 void send_signals(int signal, int pid, int interval, int number)
 {
-    dprintt("at beginning of send_signals", getpid());
+	dprintt("at beginning of send_signals", getpid());
 
-    for(int i = 1; i <= number; i++)
-    {
-        assertsyscall(sleep(interval), == 0);
-        dprintt("sending", signal);
-        dprintt("to", pid);
-        assertsyscall(kill(pid, signal), == 0)
-    }
+	for(int i = 1; i <= number; i++)
+	{
+		assertsyscall(sleep(interval), == 0);
+		dprintt("sending", signal);
+		dprintt("to", pid);
+		assertsyscall(kill(pid, signal), == 0)
+	}
 
-    dmess("at end of send_signals");
+	dmess("at end of send_signals");
 }
 
 struct sigaction *create_handler(int signum, void(*handler)(int))
 {
-    struct sigaction *action = new(struct sigaction);
+	struct sigaction *action = new(struct sigaction);
 
-    action->sa_handler = handler;
+	action->sa_handler = handler;
 
 /*
 **  SA_NOCLDSTOP
@@ -279,75 +279,99 @@ struct sigaction *create_handler(int signum, void(*handler)(int))
 **  child processes stop(i.e., when child processes  receive
 **  one of SIGSTOP, SIGTSTP, SIGTTIN or SIGTTOU).
 */
-    if(signum == SIGCHLD)
-    {
-        action->sa_flags = SA_NOCLDSTOP | SA_RESTART;
-    }
-    else
-    {
-        action->sa_flags =  SA_RESTART;
-    }
+	if(signum == SIGCHLD)
+	{
+		action->sa_flags = SA_NOCLDSTOP | SA_RESTART;
+	}
+	else
+	{
+		action->sa_flags =  SA_RESTART;
+	}
 
-    sigemptyset(&(action->sa_mask));
-    assert(sigaction(signum, action, NULL) == 0);
-    return(action);
+	sigemptyset(&(action->sa_mask));
+	assert(sigaction(signum, action, NULL) == 0);
+	return(action);
 }
 
 void scheduler(int signum)
 {
-    WRITES("---- entering scheduler\n");
-    assert(signum == SIGALRM);
-    sys_time++;
+	WRITES("---- entering scheduler\n");
+	assert(signum == SIGALRM);
+	sys_time++;
+	
+	if(running->state != TERMINATED) 
+	{
+		running->interrupts++;
+		assert(kill(running->pid, SIGINT) == 0);
+	}
 
-    PCB* tocont = idle;
+	for(int i = 1; i <= processes.size(); i++)
+	{
+		running = processes.front();
+		processes.pop_front();
+		processes.push_back(running);
+		if(running->state == NEW)
+		{
+			int child = fork();
+			assert(child >= 0);
+			if(child == 0)
+			{
+				execl(running->name, running->name, nullptr);
+			}
+			running->pid = child;
+		}
+			
+	}
+	//running = idle;
 
-    WRITES("continuing");
-    WRITEI(tocont->pid);
-    WRITES("\n");
+	WRITES("continuing");
+	WRITEI(running->pid);
+	WRITES("\n");
 
-    tocont->state = RUNNING;
-    if(kill(tocont->pid, SIGCONT) == -1)
-    {
-        WRITES("in sceduler kill error: ");
-        WRITEI(errno);
-        WRITES("\n");
-        return;
-    }
-    WRITES("---- leaving scheduler\n");
+	running->state = RUNNING;
+	if(kill(running->pid, SIGCONT) == -1)
+	{
+		WRITES("in sceduler kill error: ");
+		WRITEI(errno);
+		WRITES("\n");
+		return;
+	}
+	WRITES("---- leaving scheduler\n");
 }
 
 void process_done(int signum)
 {
-    assert(signum == SIGCHLD);
-    WRITES("---- entering process_done\n");
+	assert(signum == SIGCHLD);
+	WRITES("---- entering process_done\n");
 
-    // might have multiple children done.
-    for(;;)
-    {
-        int status, cpid;
+	// might have multiple children done.
+	for(EVER)
+	{
+		int status, cpid;
 
-        // we know we received a SIGCHLD so don't wait.
-        cpid = waitpid(-1, &status, WNOHANG);
+		// we know we received a SIGCHLD so don't wait.
+		cpid = waitpid(-1, &status, WNOHANG);
 
-        if(cpid < 0)
-        {
-            WRITES("cpid < 0\n");
-            assertsyscall(kill(0, SIGTERM), != 0);
-        }
-        else if(cpid == 0)
-        {
-            // no more children.
-            break;
-        }
-        else
-        {
-            WRITES("process exited: ");
-            WRITEI(cpid);
-            WRITES("\n");
-        }
-    }
+		if(cpid < 0)
+		{
+			WRITES("cpid < 0\n");
+			assertsyscall(kill(0, SIGTERM), != 0);
+		}
+		else if(cpid == 0)
+		{
+			// no more children.
+			break;
+		}
+		else
+		{
+			WRITES("process exited: ");
+			WRITEI(cpid);
+			WRITES("\n");
+		running->state = TERMINATED;
+		}
+	}
 
-    WRITES("---- leaving process_done\n");
+	WRITES("---- leaving process_done\n");
 }
 
 
@@ -356,56 +380,56 @@ void process_done(int signum)
 */
 void boot()
 {
-    sys_time = 0;
+	sys_time = 0;
 
-    ISV[SIGALRM] = scheduler;
-    ISV[SIGCHLD] = process_done;
-    struct sigaction *alarm = create_handler(SIGALRM, ISR);
-    struct sigaction *child = create_handler(SIGCHLD, ISR);
+	ISV[SIGALRM] = scheduler;
+	ISV[SIGCHLD] = process_done;
+	struct sigaction *alarm = create_handler(SIGALRM, ISR);
+	struct sigaction *child = create_handler(SIGCHLD, ISR);
 
-    // start up clock interrupt
-    int ret;
-    if((ret = fork()) == 0)
-    {
-        send_signals(SIGALRM, getppid(), 1, NUM_SECONDS);
+	// start up clock interrupt
+	int ret;
+	if((ret = fork()) == 0)
+	{
+		send_signals(SIGALRM, getppid(), 1, NUM_SECONDS);
 
-        // once that's done, cleanup and really kill everything...
-        delete(alarm);
-        delete(child);
-        delete(idle);
-        kill(0, SIGTERM);
-    }
+		// once that's done, cleanup and really kill everything...
+		delete(alarm);
+		delete(child);
+		delete(idle);
+		kill(0, SIGTERM);
+	}
 
-    if(ret < 0)
-    {
-        perror("fork");
-    }
+	if(ret < 0)
+	{
+		perror("fork");
+	}
 }
 
 void create_idle()
 {
-    idle = new(PCB);
-    idle->state = READY;
-    idle->name = "IDLE";
-    idle->ppid = getpid();
-    idle->interrupts = 0;
-    idle->switches = 0;
-    idle->started = sys_time;
+	idle = new(PCB);
+	idle->state = READY;
+	idle->name = "IDLE";
+	idle->ppid = getpid();
+	idle->interrupts = 0;
+	idle->switches = 0;
+	idle->started = sys_time;
 
-    if((idle->pid = fork()) == 0)
-    {
-        pause();
-        perror("pause in create_idle");
-    }
+	if((idle->pid = fork()) == 0)
+	{
+		pause();
+		perror("pause in create_idle");
+	}
 }
 
 int main(int argc, char **argv)
 {
-    boot();
+	boot();
 
-    create_idle();
-    running = idle;
-    cout << running;
+	create_idle();
+	running = idle;
+	cout << running;
 	PCB* currentBlock;
 
 	//initialize PCBs	
@@ -421,12 +445,12 @@ int main(int argc, char **argv)
 		printf("pushedback\n");
 	}
 
-    // we keep this process around so that the children don't die and
-    // to keep the IRQs in place.
-    for(EVER)
-    {
-        // "Upon termination of a signal handler started during a
-        // pause(), the pause() call will return."
-        pause();
-    }
+	// we keep this process around so that the children don't die and
+	// to keep the IRQs in place.
+	for(EVER)
+	{
+		// "Upon termination of a signal handler started during a
+		// pause(), the pause() call will return."
+		pause();
+	}
 }
